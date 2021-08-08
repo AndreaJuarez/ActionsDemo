6fea833b6a1dc157766e119d6034d638bb5a0a0e
AndreaJuarez <1718110394@utectulancingo.edu.mx>
2021-08-08 05:20:41 +0000

    tests version 02

--- docker/pruebas/arreglo.py
+++ docker/pruebas/arreglo.py
@@ -2,12 +2,12 @@ def order(array):
     array.sort()
     return array
 
-def count_mayor(diccionario):
+def count_mayores(diccionario):
     count = 0
     for i in diccionario:
         if i.get("edad") < 18:
             pass
         else:
             count = count+1
-    print("Hay un total de: ",count," personas mayores de edad actualmente.")
+    print("Hay un total de: ",count," personas mayores de edad.")
     return count
--- docker/pruebas/test_arreglo.py
+++ docker/pruebas/test_arreglo.py
@@ -1,13 +1,14 @@
 import pytest
-import arreglo as arreglo
+import arreglo as ar
 
 array = [5,3,4,2,1]
-diccionario = [{"nombre":"Eduardo","edad":23},
-               {"nombre":"Melissa","edad":7},
-               {"nombre":"Andrea","edad":21}]
+diccionario = [
+    {"nombre":"Eduardo","edad":23},
+    {"nombre":"Melissa","edad":7},
+    {"nombre":"Andrea","edad":21}]
 
 def test_arreglo():
-    assert arreglo.order(array) == [1,2,3,4,5]
+    assert ar.order(array) == [1,2,3,4,5]
 
-def test_count_mayor():
-    assert arreglo.count_mayor(diccionario) == 2
+def test_contar_mayores():
+    assert ar.count_mayores(diccionario) == 2
